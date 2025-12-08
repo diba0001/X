efbf59433b9e9132c432db2a50e6bd7e3527be1d
emjo0002 <143484536+emjo0002@users.noreply.github.com>
2025-12-08 16:29:04 +0100

    sql

--- x.sql
+++ x.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb
--- Generation Time: Dec 07, 2025 at 02:52 PM
+-- Generation Time: Dec 08, 2025 at 03:28 PM
 -- Server version: 10.6.20-MariaDB-ubu2004
 -- PHP Version: 8.2.27
 
@@ -32,6 +32,7 @@ CREATE TABLE `comments` (
   `comment_post_fk` char(32) NOT NULL,
   `comment_user_fk` char(32) NOT NULL,
   `comment_text` text NOT NULL,
+  `comment_updated_at` bigint(20) NOT NULL,
   `comment_created_at` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -72,8 +73,8 @@ CREATE TABLE `follows` (
 --
 
 INSERT INTO `follows` (`follow_follower_fk`, `follow_followed_fk`, `follow_timestamp`) VALUES
-('', '88a93bb5267e443eb0047f421a7a2f34', 1764581121),
-('', '9860c6174a3141c5b1e7c8b3638b2f2b', 1764855078),
+('', '88a93bb5267e443eb0047f421a7a2f34', 1765207439),
+('', '9860c6174a3141c5b1e7c8b3638b2f2b', 1765207439),
 ('9860c6174a3141c5b1e7c8b3638b2f2b', '805a39cd8c854ee8a83555a308645bf5', 1765103020),
 ('9860c6174a3141c5b1e7c8b3638b2f2b', '88a93bb5267e443eb0047f421a7a2f34', 1765100873);
 
@@ -94,9 +95,12 @@ CREATE TABLE `likes` (
 --
 
 INSERT INTO `likes` (`like_user_fk`, `like_post_fk`, `like_timestamp`) VALUES
-('', '0e541eea9ead41db881c56f4802290ba', 1765107682),
+('', '3ccd07dc9eb64fe4a9e7059ada79d61d', 1765186724),
 ('', '7d122d1016a742ea80ed7c7936f04f61', 1765104385),
+('', 'bcaa6df8880e411a9c25deaafae2314a', 1765183418),
 ('', 'c4ddb3c171a34cb2903bc98d03883d24', 1765113823),
+('', 'e428fa103e6c41c5948c553b0a64b7b9', 1765186434),
+('', 'fca0b7f700de4682909ec8372744b8fd', 1765188567),
 ('9860c6174a3141c5b1e7c8b3638b2f2b', '7d122d1016a742ea80ed7c7936f04f61', 1765104535),
 ('9860c6174a3141c5b1e7c8b3638b2f2b', 'c4ddb3c171a34cb2903bc98d03883d24', 1765102886),
 ('9860c6174a3141c5b1e7c8b3638b2f2b', 'd60b8eedfcb6420587b2f528566386df', 1765104536),
@@ -147,16 +151,16 @@ CREATE TABLE `posts` (
 
 INSERT INTO `posts` (`post_pk`, `post_user_fk`, `post_message`, `post_total_likes`, `post_total_comments`, `post_media_path`, `post_blocked_at`, `post_deleted_at`, `post_updated_at`, `post_created_at`) VALUES
 ('070678cb348846839b79d2ed6834f743', '88a93bb5267e443eb0047f421a7a2f34', 'a post with a pic', 0, 0, 'images/aa147d759fcb4ceeb7dc8d1e45939aa4_twitter_erd_crowsfoot.png', 0, 0, 1764861780, 1764861751),
-('0e541eea9ead41db881c56f4802290ba', '9860c6174a3141c5b1e7c8b3638b2f2b', 'hej', 1, 0, '', 0, 0, 0, 1765105787),
+('0e541eea9ead41db881c56f4802290ba', '9860c6174a3141c5b1e7c8b3638b2f2b', 'hej', 0, 0, '', 0, 0, 0, 1765105787),
 ('28dd4c1671634d73acd29a0ab109bef1', '805a39cd8c854ee8a83555a308645bf5', 'My first super life !', 1, 0, 'images/post_3.jpg', 0, 0, 0, 1764762106),
 ('299323cf81924589b0de265e715a1f9e', '225a9fc15b8f409aa5c8ee7eafee516b', 'test3', 1, 0, 'images/post_1.jpg', 0, 0, 0, 1764762106),
 ('2c47ae1bc8f249109157eeda70504ac5', '88a93bb5267e443eb0047f421a7a2f34', 'A picture', 0, 0, 'images/0026ecf80e9341c1b534c155f8eb6c8e_IMG_6962.jpeg', 0, 0, 0, 1764861546),
 ('2e6532f3d6084a54afd4f49a32cc24cf', '5388749', 'hi', 0, 0, '', 0, 0, 0, 1764854836),
 ('3229ec27b6a247f8b44cf3679dae85db', '9860c6174a3141c5b1e7c8b3638b2f2b', 'hej', 0, 0, '', 0, 0, 0, 1765110195),
-('3ccd07dc9eb64fe4a9e7059ada79d61d', '', 'malthe og emil dater mange gange', 0, 0, '', 0, 0, 1765116160, 1764859669),
+('3ccd07dc9eb64fe4a9e7059ada79d61d', '', 'malthe og emil dater mange gange, elsker dem', 1, 0, '', 0, 0, 1765186957, 1764859669),
 ('4634e7714feb42aea9cb055b85f352fc', '237485250205824500245', 'hi', 0, 0, '', 0, 0, 0, 1764855088),
 ('508f6200791446d49165c0cccd7d7268', '240927592750274', 'hi', 0, 0, 'images/3d76bdd0a6414c0b89a9dc7dc1c7c3f5_twitter_erd_crowsfoot.png', 0, 0, 0, 1764854203),
-('560d69eab9fc41da85f0df49df1a00ae', '', 'hej14:58', 0, 0, '', 0, 0, 0, 1765115941),
+('560d69eab9fc41da85f0df49df1a00ae', '', 'hej14:59', 0, 0, '', 0, 0, 1765183384, 1765115941),
 ('5ed0a8395020449ba2c5abe294689d3b', '88a93bb5267e443eb0047f421a7a2f34', 'emil elzker kanelgifflz', 0, 0, 'images/7e73abc8fda8474595a145387d1abbc7_favicon-16x16.png', 0, 0, 1764860941, 1764859867),
 ('663f6045b08c49aeae88ae10eb1d2625', '', 'claus', 0, 0, '', 0, 0, 0, 1764859813),
 ('7d122d1016a742ea80ed7c7936f04f61', '9860c6174a3141c5b1e7c8b3638b2f2b', 'jeg hader rasmud', 2, 0, '', 1765116505, 0, 1765100528, 1765100480),
@@ -164,15 +168,15 @@ INSERT INTO `posts` (`post_pk`, `post_user_fk`, `post_message`, `post_total_like
 ('9605bb608a324ff185fc5033744d62ad', '', 'emil elsker majskiks', 0, 0, '', NULL, 0, 0, 1764859234),
 ('99fefea24ea5419da19ed1f8cf8e9499', '225a9fc15b8f409aa5c8ee7eafee516b', 'wow', 0, 0, 'images/post_1.jpg', 1764838458, 0, 0, 1764762106),
 ('ab2f3c649a444eb588cd8307c6f8ae56', '2342304205092', 'hi', 0, 0, '', 0, 0, 0, 1764854993),
-('bcaa6df8880e411a9c25deaafae2314a', '225a9fc15b8f409aa5c8ee7eafee516b', 'test', 2, 0, '', 0, 0, 1765100282, 1764762106),
+('bcaa6df8880e411a9c25deaafae2314a', '225a9fc15b8f409aa5c8ee7eafee516b', 'test', 3, 0, '', 0, 0, 1765100282, 1764762106),
 ('c0e92aad8b6c46c6899d59ecc5e8105a', '', 'hej', 0, 0, '', NULL, 0, 0, 1764858769),
 ('c156b4470e084e529cc5189c44b9d030', '', 'emil', 0, 0, '', NULL, 0, 0, 1764858807),
 ('c4dca608ab1c4e788d34a818658313ed', '235702945879450', 'hi', 0, 0, 'images/66b1f63a3bdd45e28805007e28eb02cd_twitter_erd_crowsfoot.png', 0, 0, 0, 1764854012),
 ('c4ddb3c171a34cb2903bc98d03883d24', '', 'hi', 2, 0, '', 0, 0, 0, 1764854975),
-('d60b8eedfcb6420587b2f528566386df', '', 'hi3', 1, 0, 'images/666c6234a4254807b73e134781ec282a_android-chrome-512x512.png', 0, 0, 1765108984, 1764854549),
-('e428fa103e6c41c5948c553b0a64b7b9', '', 'emil er zuper zød x2', 0, 0, '', 0, 0, 1765108718, 1764859321),
+('d60b8eedfcb6420587b2f528566386df', '', 'hi2', 1, 0, 'images/666c6234a4254807b73e134781ec282a_android-chrome-512x512.png', 0, 0, 1765188829, 1764854549),
+('e428fa103e6c41c5948c553b0a64b7b9', '', 'emil er zuper zød x4', 1, 0, '', 0, 0, 1765186451, 1764859321),
 ('e4652c7659fa456babd12e27c33155d4', '', 'hi 2', 0, 0, '', 0, 0, 1764855574, 1764855567),
-('fca0b7f700de4682909ec8372744b8fd', '', 'jeg laver et post', 1, 0, '', 0, 0, 0, 1764857775);
+('fca0b7f700de4682909ec8372744b8fd', '', 'jeg laver et post', 2, 0, '', 0, 0, 1765196210, 1764857775);
 
 -- --------------------------------------------------------
 
@@ -204,7 +208,6 @@ CREATE TABLE `users` (
   `user_pk` char(32) NOT NULL,
   `user_email` varchar(100) NOT NULL,
   `user_password` varchar(255) NOT NULL,
-  `user_password_reset_key` char(32) NOT NULL,
   `user_username` varchar(20) NOT NULL,
   `user_first_name` varchar(20) NOT NULL,
   `user_last_name` varchar(20) NOT NULL DEFAULT '',
@@ -226,6 +229,7 @@ INSERT INTO `users` (`user_pk`, `user_email`, `user_password`, `user_username`, 
 ('6b48c6095913402eb4841529830e5415', 'a@a1.com', 'scrypt:32768:8:1$rRjuDGIwaA31YlPi$f73f9a059fb3757ba6724d9c94e2a192d8b8d59fcd18d7b11c57e508f1b9cfb94bb7c6fd4f8d632b777e31cd47aef9c95adcad2451786cbb7e7c073fe8cbaf3a', 'santiago1', 'Santiago', '', 'static/images/avatars/avatar_3.jpg', 'ee92b2c86a6c48569138a43ce8bc1d48', 0, 0, 0, 0),
 ('805a39cd8c854ee8a83555a308645bf5', 'fullflaskdemomail@gmail.com', 'scrypt:32768:8:1$VlBgiW1xFsZuKRML$a5f61d62ac3f45d42c58cf8362637e717793b8760f026b1b47b7bfec47037abbe13e1c20e8bdc66fc03cc153d0bcf6185e15cf25ad58eb9d344267882dd7e78c', 'santiago', 'Santiago', '', 'static/images/avatars/avatar_4.jpg', '', 565656, 0, 0, 0),
 ('88a93bb5267e443eb0047f421a7a2f34', 'santi@gmail.com', 'scrypt:32768:8:1$PEIO0eliDPqnCCbw$acb791128831bc90030ac363e4b76db196689bd99c1ccde5c2c20a7d4fe909e07129f3f4fd4f086e347375edbb8229e9ba5dc126cc14f6107fb1fc2abf6498f8', 'gustav', 'Gustav', '', 'static/images/avatars/f6262d530c0d46a48710087334157593.png', '', 54654564, 0, 0, 0),
+('9225aa8a911c44ff86a1598d852adb98', 'emil.johansson2@gmail.com', 'scrypt:32768:8:1$0OHMg1epYFs0bbh1$37a720ea2e75548e5fa043eeab4fad2691918a3eb21a021f613d03ff6e53ed8e61db9a3d205c3c7b867e7ca926dca25bb66610f91a304ef3178b068529e764dc', 'emil8883', 'emil', '', 'https://avatar.iran.liara.run/public/40', '', 1765200933, 1765201014, 0, 0),
 ('9860c6174a3141c5b1e7c8b3638b2f2b', 'maltheaaen@gmail.com', 'scrypt:32768:8:1$5NSH8Gsqi83lQV24$b61989755f5e00e7632463dee7b806b93acab7d4de36b6e32caf47a2fcef8bf23db0624a3767d5bae3ba40c77673171dad51a4b472e44a9463fc141a0b7f37bb', 'Malt', 'Malthe', '', 'static/images/avatars/a646c5c464854943b27d7c07c0074fb8.png', '', 54654564, 0, 0, 0);
 
 --
